e2e0791ec1339de175879a6930525d50ae0a4436
isuri97 <isurianuradha96@gmail.com>
2018-06-15 05:08:36 +0530

    Adding internal component to default authenticator.

---
+++ components/org.wso2.carbon.identity.sample.default.authenticator/src/main/java/org/wso2/custom/authenticator/local/internal/BasicAServiceComponent.java
@@ -0,0 +1,56 @@
+
+package org.wso2.custom.authenticator.local.internal;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.osgi.service.component.ComponentContext;
+import org.wso2.custom.authenticator.local.BasicCustomAuthenticator;
+import org.wso2.carbon.identity.application.authentication.framework.ApplicationAuthenticator;
+import org.wso2.carbon.user.core.service.RealmService;
+
+
+/**
+ * @scr.component name="org.wso2.custom.authenticator.local.basic.component" immediate="true"
+ * @scr.reference name="realm.service"
+ * interface="org.wso2.carbon.user.core.service.RealmService"cardinality="1..1"
+ * policy="dynamic" bind="setRealmService" unbind="unsetRealmService"
+ */
+public class BasicAServiceComponent {
+
+
+    private static Log log = LogFactory.getLog(BasicAServiceComponent.class);
+
+    private static RealmService realmService;
+
+    public static RealmService getRealmService() {
+        return realmService;
+    }
+
+    protected void activate(ComponentContext ctxt) {
+        try {
+            BasicCustomAuthenticator basicCustomAuth = new BasicCustomAuthenticator();
+            ctxt.getBundleContext().registerService(ApplicationAuthenticator.class.getName(), basicCustomAuth, null);
+            if (log.isDebugEnabled()) {
+                log.info("BasicCustomAuthenticator bundle is activated");
+            }
+        } catch (Throwable e) {
+            log.error("BasicCustomAuthenticator bundle activation Failed", e);
+        }
+    }
+
+    protected void deactivate(ComponentContext ctxt) {
+        if (log.isDebugEnabled()) {
+            log.info("BasicCustomAuthenticator bundle is deactivated");
+        }
+    }
+
+    protected void unsetRealmService(RealmService realmService) {
+        log.debug("UnSetting the Realm Service");
+        BasicAServiceComponent.realmService = null;
+    }
+
+    protected void setRealmService(RealmService realmService) {
+        log.debug("Setting the Realm Service");
+        BasicAServiceComponent.realmService = realmService;
+    }
+}
