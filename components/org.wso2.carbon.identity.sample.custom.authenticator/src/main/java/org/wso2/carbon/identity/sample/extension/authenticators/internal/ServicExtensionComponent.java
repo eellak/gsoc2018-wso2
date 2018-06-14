754913ae7bce87157b983b70de049143c2aa501d
isuri97 <isurianuradha96@gmail.com>
2018-06-14 22:37:28 +0530

    Adding internal component for custom authenticator.

---
+++ components/org.wso2.carbon.identity.sample.custom.authenticator/src/main/java/org/wso2/carbon/identity/sample/extension/authenticators/internal/ServicExtensionComponent.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.sample.extension.authenticators.internal;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.osgi.service.component.ComponentContext;
+import org.wso2.carbon.identity.application.authentication.framework.ApplicationAuthenticator;
+import org.wso2.carbon.identity.sample.extension.authenticators.SampleAuthenticator;
+
+/**
+ * @scr.component name="identity.sample.auth.extension.component" immediate="true"
+ */
+public class ServicExtensionComponent {
+
+    private static Log log = LogFactory.getLog(ServicExtensionComponent.class);
+
+    /**
+     * @param ctxt
+     */
+    protected void activate(ComponentContext ctxt) {
+
+        try {
+            log.info("The Sample Authenticator extension is activated."
+                    + " Provides some sample authenticators for demonstration purposes.");
+
+            SampleAuthenticator sampleAuthenticator = new SampleAuthenticator();
+            ctxt.getBundleContext()
+                    .registerService(ApplicationAuthenticator.class.getName(), sampleAuthenticator, null);
+            if (log.isDebugEnabled()) {
+                log.info("BasicCustomAuthenticator bundle is activated");
+            }
+        } catch (Throwable e) {
+            log.error("BasicCustomAuthenticator bundle activation Failed", e);
+        }
+
+    }
+
+    protected void deactivate(ComponentContext ctxt) {
+
+        if (log.isDebugEnabled()) {
+            log.info("BasicCustomAuthenticator bundle is deactivated");
+        }
+    }
+
+}
